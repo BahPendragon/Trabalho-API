f0367b4cad6bb4a86579ab29e6ee54201c27859a
André Ricardo Adami Drummond Filho <81425108+BahPendragon@users.noreply.github.com>
2021-05-31 13:16:56 -0300

    Update ClienteController.java

--- src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
+++ src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,11 +26,8 @@ public class ClienteController {
     private ClienteRepository _repositorioCliente;
 
     @GetMapping
-    public ResponseEntity<Cliente> obterTodos(@RequestBody Cliente cliente){
-
-        var obter = this._repositorioCliente.findAll(cliente);
-
-        return new ResponseEntity<>(obter, HttpStatus.OK);
+    public List<Cliente> obterTodos(@RequestBody Cliente cliente){
+        return this._repositorioCliente.findAll();
     }
 	
         
@@ -51,46 +49,45 @@ public class ClienteController {
 	
 	
     @PostMapping
-    public ResponseEntity<Cliente> adicionar(@RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> adicionar(@RequestBody Cliente cliente) {
 
-    var adicionado = this._repositorioCliente.save(cliente);
+        var adicionado = this._repositorioCliente.save(cliente);
 
-    return new ResponseEntity<Cliente>(adicionado, HttpStatus.CREATED);		
+        return new ResponseEntity<Cliente>(adicionado, HttpStatus.CREATED);		
 
 }
     
     @DeleteMapping("/{id}")
-    public ResponseEntity<Optional<Cliente>> deletar(@PathVariable(value = "id") Long id) {
-	try {
+	public ResponseEntity<Optional<Cliente>> deletar(@PathVariable(value = "id") Long id) {
+		try {
 			
-	     var deletado = this._repositorioCliente.deleteById(id);
+        this._repositorioCliente.deleteById(id);
 		
-	     return new ResponseEntity<Optional<Cliente>>(deletado, HttpStatus.OK);
+		return new ResponseEntity<Optional<Cliente>>(HttpStatus.OK);
 		
-	} catch (ClienteNaoEncontratoException e) {
+		} catch (ClienteNaoEncontratoException e) {
 			
-	        System.out.println(e.getMessage()); 
+			System.out.println(e.getMessage()); 
 			
-                return new ResponseEntity<Optional<Cliente>>(HttpStatus.NOT_FOUND);
-       }
-}
-
-       @PutMapping("/{id}")
-       public ResponseEntity<Cliente> atualizar(@PathVariable(value = "id") Long id, @RequestBody Cliente cliente) {
-		
-	    try {
-		 cliente.setId(id);
-			
-		 var clienteAtualizado = this._repositorioCliente.save(cliente);
-			
-		 return new ResponseEntity<>(clienteAtualizado, HttpStatus.OK);
-			
-	 } catch (ClienteNaoEncontratoException e) {
-		    
-		System.out.println(e.getMessage()); 
-			
-		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<Optional<Cliente>>(HttpStatus.NOT_FOUND);
+		}
 	}
-  
-     }
-}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable(value = "id") Long id, @RequestBody Cliente cliente) {
+		
+		try {
+			cliente.setId(id);
+			
+			var clienteAtualizado = this._repositorioCliente.save(cliente);
+			
+			return new ResponseEntity<>(clienteAtualizado, HttpStatus.OK);
+			
+		} catch (ClienteNaoEncontratoException e) {
+			System.out.println(e.getMessage()); 
+			
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	}
