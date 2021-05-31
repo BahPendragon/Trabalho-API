fcd47371f17eae7f99279c68872c4e3c0ef009ec
André Ricardo Adami Drummond Filho <81425108+BahPendragon@users.noreply.github.com>
2021-05-31 13:09:24 -0300

    Update ProdutoRepository.java

--- src/main/java/br/com/serratec/trabalho/repository/ProdutoRepository.java
+++ src/main/java/br/com/serratec/trabalho/repository/ProdutoRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.serratec.trabalho.model.Cliente;
 import br.com.serratec.trabalho.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
