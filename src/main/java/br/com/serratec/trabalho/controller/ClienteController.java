002e831a4deb64bd7fce5aa895bf8fbd27e32675
Amanda Melo <amandamelo86@gmail.com>
2021-05-31 11:49:29 -0300

    Fiz o método adicionar com POST.

--- src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
+++ src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
@@ -1,5 +1,28 @@
 package br.com.serratec.trabalho.controller;
 
-public class ClienteController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.serratec.trabalho.model.Produto;
+import br.com.serratec.trabalho.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ClienteController {
+    @Autowired
+	private ProdutoRepository _repositorioProduto ;
+	
+	
+	@PostMapping
+	public Produto adicionar(@RequestBody Produto produto) {
+		return this._repositorioProduto.save(produto);
+	}
+	
+	
+	
 }
