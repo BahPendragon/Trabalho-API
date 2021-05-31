cb83f7d36975f97fb999262002c9e829284c016d
Dudunaitor <82180142+Dudunaitor@users.noreply.github.com>
2021-05-31 12:45:51 -0300

    Add files via upload

--- src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
+++ src/main/java/br/com/serratec/trabalho/controller/ClienteController.java
@@ -1,39 +1,38 @@
 package br.com.serratec.trabalho.controller;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.trabalho.model.Cliente;
-import br.com.serratec.trabalho.model.Produto;
 import br.com.serratec.trabalho.repository.ClienteRepository;
 
-@RequestMapping("/api/Trabalho-final")
 @RestController
+@RequestMapping("api/clitentes")
 public class ClienteController {
-	@Autowired 
-	private ClienteRepository _repositorioCliente;
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Optional<Cliente>> obter(@PathVariable(value = "id") Long id){
-		
-		 try { 
-  		   var encontrado = _repositorioCliente.findById(id);
-  		   
-  		    return new ResponseEntity<Optional<Cliente>>(encontrado, HttpStatus.OK);
-  		    
-  	 } catch (ClienteNaoEncontradoException e) {
-  		 
-  		 System.out.println(e.getMessage());
-  		 
-  		 return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-  	 }
-}
-	}
 
+    @Autowired
+    private ClienteRepository _repositorioCliente;
+
+
+    /*
+    @GetMapping
+    public List<Cliente> obterTodos(){
+        return this._repositorioCliente.findAll;
+    }
+    */
+
+    @GetMapping
+    public ResponseEntity<Cliente> obterTodos(@RequestBody Cliente cliente){
+
+        var obter = this._repositorioCliente.findAll(cliente);
+
+        return new ResponseEntity<>(obter, HttpStatus.OK);
+    }
+}
--- src/main/java/br/com/serratec/trabalho/controller/ProdutoController.java
+++ src/main/java/br/com/serratec/trabalho/controller/ProdutoController.java
@@ -1,39 +1,41 @@
 package br.com.serratec.trabalho.controller;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.serratec.trabalho.model.Produto;
 import br.com.serratec.trabalho.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
-     @Autowired
-     private ProdutoRepository _repositorioProduto;
-     
-     @GetMapping("/{id}")
-     public ResponseEntity<Optional<Produto>> obter(@PathVariable(value = "id") Integer id){
- 		
-    	 try { 
-    		   var encontrado = _repositorioProduto.findById(id);
-    		   
-    		    return new ResponseEntity<Optional<Produto>>(encontrado, HttpStatus.OK);
-    		    
-    	 } catch (ProdutoNaoEncontradoException e) {
-    		 
-    		 System.out.println(e.getMessage());
-    		 
-    		 return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-    	 }
- 
- 	}
-     
-}
 
+
+    @Autowired
+    private ProdutoRepository _repositorioProduto;
+
+    /*
+    @GetMapping
+    public List<Produto> obterTodos(){
+        return this._repositorioProduto.findAll();
+    }
+    */
+
+    @GetMapping
+    public ResponseEntity<Produto> obterTodos(@RequestBody Produto produto){
+
+        var obter = this._repositorioProduto.findAll(produto);
+
+        return new ResponseEntity<>(obter, HttpStatus.OK);
+    }
+
+
+
+}
