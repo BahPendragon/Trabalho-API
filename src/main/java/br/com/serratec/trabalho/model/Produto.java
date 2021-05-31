49e7a44f078841a834494319a888ffaac27f65fc
Daniel Bevilacqua <81425230+Magnnetto@users.noreply.github.com>
2021-05-31 12:54:36 -0300

    Update Produto.java

--- src/main/java/br/com/serratec/trabalho/model/Produto.java
+++ src/main/java/br/com/serratec/trabalho/model/Produto.java
@@ -1,12 +1,28 @@
 package br.com.serratec.trabalho.model;
 
-public class Produto {
-	private Long id;
-	private String nome;
-	private Integer quantidade;
-	private Double valor;
-	private String observacao;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String nome;
+	@NotNull
+	private Integer quantidade;
+	@NotNull
+	private Double valor;
+	@NotNull
+	private String observacao;
 	public Produto() {}
 
 	public Produto(Long id, String nome, Integer quantidade, Double valor, String observacao) {
