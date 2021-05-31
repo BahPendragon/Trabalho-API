f2f6c33d94c5f0ecfe961e95fd7b5969dc4923b4
André Ricardo Adami Drummond Filho <81425108+BahPendragon@users.noreply.github.com>
2021-05-31 13:04:47 -0300

    Update ProdutoController.java

--- src/main/java/br/com/serratec/trabalho/controller/ProdutoController.java
+++ src/main/java/br/com/serratec/trabalho/controller/ProdutoController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,6 +73,22 @@ public class ProdutoController {
     	 }
  
     }
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable(value = "id") Long id, @RequestBody Produto produto) {
+		
+		try {
+			produto.setId(id);
+			
+			var clienteAtualizado = this._repositorioProduto.save(produto);
+			
+			return new ResponseEntity<>(clienteAtualizado, HttpStatus.OK);
+			
+		} catch (ProdutoNaoEncontratoException e) {
+			System.out.println(e.getMessage()); 
+			
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
      
 }
-
